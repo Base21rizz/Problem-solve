24fd73ce475604381af4313fc0510ccc4931f100
Base21rizz <shoumikislamofficial@gmail.com>
2025-03-03 21:51:32 +0600

    dhjfk

---
+++ Week7/Day4/A_HQ_9.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define pb push_back
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9')
+        {
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
