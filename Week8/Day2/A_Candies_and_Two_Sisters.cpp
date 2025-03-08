cb9241843d41ac870c831cbe89ad01752271bbc9
Base21rizz <shoumikislamofficial@gmail.com>
2025-03-09 02:45:44 +0600

    dfkj

---
+++ Week8/Day2/A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define pb push_back
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int tc; cin >> tc;
+    while (tc--) 
+    {
+        int n;
+        cin >> n;
+        cout << (n-1)/2 << endl;
+    }
+    return 0;
+}
