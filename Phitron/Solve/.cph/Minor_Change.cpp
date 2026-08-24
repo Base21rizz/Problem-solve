fef5993ae7b2d2ce216e5924fa1cc9824aef9a0c
Base21rizz <shoumikislamofficial@gmail.com>
2026-08-24 22:05:21 +0600

    phitron problem solve xpsc

---
+++ Phitron/Solve/.cph/Minor_Change.cpp
@@ -0,0 +1,44 @@
+/*
+===============================================================================
+
+██████╗  █████╗ ███████╗███████╗██████╗  ██╗██████╗ ██╗███████╗███████╗
+██╔══██╗██╔══██╗██╔════╝██╔════╝╚════██╗███║██╔══██╗██║╚══███╔╝╚══███╔╝
+██████╔╝███████║███████╗█████╗   █████╔╝╚██║██████╔╝██║  ███╔╝   ███╔╝
+██╔══██╗██╔══██║╚════██║██╔══╝  ██╔═══╝  ██║██╔══██╗██║ ███╔╝   ███╔╝
+██████╔╝██║  ██║███████║███████╗███████╗ ██║██║  ██║██║███████╗███████╗
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝
+
+===============================================================================
+*/
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define pb push_back
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string a, b;
+    cin >> a >> b;
+    int asize = a.size();
+    int bsize = b.size();
+    int count = 0;
+    int diff = min(asize, bsize);
+    for (int i = 0; i < diff; i++)
+    {
+        if (a[i] != b[i])
+            count++;
+    }
+    if (asize == bsize)
+        cout << count << endl;
+    else
+        cout << count + (asize - bsize) << endl;
+
+    return 0;
+}
---
+++ Phitron/Solve/.cph/Remove_It.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define pb push_back
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, x;
+    cin >> n >> x;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != x)
+            cout << arr[i] << " ";
+    }
+    return 0;
+}
