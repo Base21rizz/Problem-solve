83e20b80430df19448975c9a2bd8af66e65af720
Base21rizz <shoumikislamofficial@gmail.com>
2026-09-06 19:32:22 +0600

    Phitron Solve

---
+++ Phitron/Solve/Chat_Order.cpp
@@ -0,0 +1,53 @@
+/*
+===============================================================================
+
+██████╗  █████╗ ███████╗███████╗██████╗  ██╗██████╗ ██╗███████╗███████╗
+██╔══██╗██╔══██╗██╔════╝██╔════╝╚════██╗███║██╔══██╗██║╚══███╔╝╚══███╔╝
+██████╔╝███████║███████╗█████╗   █████╔╝╚██║██████╔╝██║  ███╔╝   ███╔╝
+██╔══██╗██╔══██║╚════██║██╔══╝  ██╔═══╝  ██║██╔══██╗██║ ███╔╝   ███╔╝
+██████╔╝██║  ██║███████║███████╗███████╗ ██║██║  ██║██║███████╗███████╗
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝
+
+===============================================================================
+*/
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define pb push_back
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    stack<string> st;
+    map<string, int> mp;
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        st.push(s);
+        mp[s]++;
+    }
+    while (!st.empty())
+    {
+        if (mp.find(st.top()) != mp.end())
+        {
+            cout << st.top() << endl;
+            mp.erase(st.top());
+            st.pop();
+        }
+        else
+        {
+            st.pop();
+        }
+    }
+
+    return 0;
+}
