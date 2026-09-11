c016cbfe9b3474d81cde7ef5a98dccbde0d5e0f3
Base21rizz <shoumikislamofficial@gmail.com>
2026-09-11 15:06:09 +0600

    CF upsolve

---
+++ 09-26/D_MEX_Multiset.cpp
@@ -0,0 +1,66 @@
+/*
+===============================================================================
+
+██████╗  █████╗ ███████╗███████╗██████╗  ██╗██████╗ ██╗███████╗███████╗
+██╔══██╗██╔══██╗██╔════╝██╔════╝╚════██╗███║██╔══██╗██║╚══███╔╝╚══███╔╝
+██████╔╝███████║███████╗█████╗   █████╔╝╚██║██████╔╝██║  ███╔╝   ███╔╝
+██╔══██╗██╔══██║╚════██║██╔══╝  ██╔═══╝  ██║██╔══██╗██║ ███╔╝   ███╔╝
+██████╔╝██║  ██║███████║███████╗███████╗ ██║██║  ██║██║███████╗███████╗
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝
+
+===============================================================================
+*/
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define pb push_back
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        int zero = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+            if (v[i] == 0)
+                zero++;
+        }
+        bool aprint = true;
+        if (zero == 1)
+            cout << "NO" << endl;
+        else
+        {
+            cout << "YES" << endl;
+            for (int i = 0; i < n; i++)
+            {
+                if (v[i] == 0)
+                {
+                    if (aprint)
+                    {
+                        cout << 'A';
+                        aprint = false;
+                    }
+                    else
+                        cout << 'B';
+                }
+                else
+                    cout << 'C';
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
