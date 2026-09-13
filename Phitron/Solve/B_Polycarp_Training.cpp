9f3bed539f506e03a23a89be03777ba37c8fb1c7
Base21rizz <shoumikislamofficial@gmail.com>
2026-09-13 20:45:14 +0600

    phitron solve

---
+++ Phitron/Solve/B_Polycarp_Training.cpp
@@ -0,0 +1,52 @@
+/*
+===============================================================================
+
+██████╗  █████╗ ███████╗███████╗██████╗  ██╗██████╗ ██╗███████╗███████╗
+██╔══██╗██╔══██╗██╔════╝██╔════╝╚════██╗███║██╔══██╗██║╚══███╔╝╚══███╔╝
+██████╔╝███████║███████╗█████╗   █████╔╝╚██║██████╔╝██║  ███╔╝   ███╔╝
+██╔══██╗██╔══██║╚════██║██╔══╝  ██╔═══╝  ██║██╔══██╗██║ ███╔╝   ███╔╝
+██████╔╝██║  ██║███████║███████╗███████╗ ██║██║  ██║██║███████╗███████╗
+╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝
+
+===============================================================================
+*/
+#include <bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define pb push_back
+using namespace std;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    multiset<int> ms;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        ms.insert(x);
+    }
+    int ans = 0, day = 1;
+    while (true)
+    {
+        auto it = ms.lower_bound(day);
+        if (it != ms.end())
+        {
+            ans++;
+            ms.erase(it);
+        }
+        else
+            break;
+        day++;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
